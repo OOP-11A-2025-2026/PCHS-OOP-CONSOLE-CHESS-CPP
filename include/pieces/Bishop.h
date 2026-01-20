f6e6723745e8883780a3ae264a25a0638b214809
YordanTsonev <yordantsonev20@gmail.com>
2026-01-20 16:17:36 +0200

    Adding the pieces

---
+++ include/pieces/Bishop.h
@@ -0,0 +1,14 @@
+#ifndef BISHOP_H
+#define BISHOP_H
+
+#include "pieces/Piece.h"
+
+class Bishop : public Piece {
+public:
+    Bishop(Color color, int file, int rank);
+
+    std::vector<Move> getLegalMoves(const Board& board) const override;
+    Piece* clone() const override;
+};
+
+#endif // BISHOP_H
